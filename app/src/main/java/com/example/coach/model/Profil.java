721dea75d0d96edbed9018235625750aa2563224
Nathan Boudier <nathan.boudier@gmail.com>
2026-03-11 16:07:08 +0100

    S2bis : doc technique et tests fonctionnels.

--- app/src/main/java/com/example/coach/model/Profil.java
+++ app/src/main/java/com/example/coach/model/Profil.java
@@ -1,5 +1,8 @@
 package com.example.coach.model;
 
+/**
+ * Classe métier contenant les informations d'un profil
+ */
 public class Profil {
     // Constantes pour les bornes IMG
     private static final int minFemme = 25;
@@ -7,7 +10,13 @@ public class Profil {
     private static final int minHomme = 15;
     private static final int maxHomme = 20;
 
+    /**
+     * messages à afficher suivant la valeur de l'img
+     */
     private static final String[] message = {"trop faible", "normal", "trop élevé"};
+    /**
+     * nom du fichier drawable correspondant à la valeur de l'img
+     */
     private static final String[] image = {"maigre", "normal", "graisse"};
 
     // Propriétés d'entrée
@@ -18,30 +27,58 @@ public class Profil {
     private  double img;
     private int indice;
 
+    /**
+     * Constructeur : valorise les propriétés
+     * @param poids
+     * @param taille
+     * @param age
+     * @param sexe
+     */
     public Profil(Integer poids, Integer taille, Integer age, Integer sexe) {
         this.poids = poids;
         this.taille = taille;
         this.age = age;
         this.sexe = sexe;
         this.img = calculImg();
+        this.indice = calcultIndice();
     }
 
+    /**
+     *
+     * @return nom du fichier drawable qui correspond à l'img
+     */
     public String getImage(){
         return image[indice];
     }
 
+    /**
+     *
+     * @return message à afficher qui correspond à l'img
+     */
     public String getMessage(){
         return message[indice];
     }
 
+    /**
+     *
+     * @return vrai si l'img est normal
+     */
     public boolean normal(){
         return indice == 1;
     }
 
+    /**
+     *
+     * @return valeur de l'img
+     */
     public double getImg() {
         return img;
     }
 
+    /**
+     * Calcul de l'img
+     * @return résultat du calcul de l'img
+     */
     private double calculImg(){
         double tailleMetres = taille / 100.0;
         return (1.2 * poids / (tailleMetres * tailleMetres))
@@ -50,6 +87,11 @@ public class Profil {
                 - 5.4;
     }
 
+    /**
+     * Calcule l'indice correspondant à l'img
+     * pour se positionner dans la bonne case des tableaux message et image
+     * @return valeur de l'indice
+     */
     private int calcultIndice() {
         int min = (sexe == 0) ? minFemme : minHomme;
         int max = (sexe == 0) ? maxFemme : maxHomme;
--- app/src/main/java/com/example/coach/view/MainActivity.java
+++ app/src/main/java/com/example/coach/view/MainActivity.java
@@ -19,6 +19,9 @@ import com.example.coach.R;
 import com.example.coach.contract.ICalculView;
 import com.example.coach.presenter.CalculPresenter;
 
+/**
+ * Activity qui permet le calcul de l'img
+ */
 public class MainActivity extends AppCompatActivity implements ICalculView {
 
     private EditText txtPoids;
@@ -45,12 +48,18 @@ public class MainActivity extends AppCompatActivity implements ICalculView {
         init();
     }
 
+    /**
+     * Traitements nécessaires dès la création de l'activity
+     */
     private void init(){
         chargeObjetsGraphiques();
         presenter = new CalculPresenter(this);
         btnCalc.setOnClickListener(v -> btnCalc_clic());
     }
 
+    /**
+     * Récupération des objets graphiques
+     */
     private void chargeObjetsGraphiques(){
         txtPoids = findViewById(R.id.txtPoids);
         txtTaille = findViewById(R.id.txtTaille);
@@ -61,6 +70,9 @@ public class MainActivity extends AppCompatActivity implements ICalculView {
         btnCalc = findViewById(R.id.btnCalc);
     }
 
+    /**
+     * Traitements réalisés lors du clic sur le bouton Calculer
+     */
     private void btnCalc_clic(){
         Integer poids = 0, taille = 0, age = 0, sexe = 0;
         try {
@@ -80,6 +92,13 @@ public class MainActivity extends AppCompatActivity implements ICalculView {
         }
     }
 
+    /**
+     * Méthode permettant l'affichage du résultat du calcul de l'img
+     * @param image nom du fichier drawable pour le smiley
+     * @param img valeur de l'img calculé
+     * @param message information textuelle correspondant à l'img
+     * @param normal vrai si l'img est normal
+     */
     @Override
     public void afficherResultat(String image, double img, String message, boolean normal) {
         int imageId = getResources().getIdentifier(image, "drawable", getPackageName());
